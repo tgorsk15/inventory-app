d5c5aab488b7f78ccd792694846798e730f4e7b2
tgorsk15 <tgbusiness463@gmail.com>
2024-08-15 17:16:17 -0500

    Add validation to new Item form

--- controllers/itemsController.js
+++ controllers/itemsController.js
@@ -3,8 +3,27 @@ const asyncHandler = require("express-async-handler")
 const categoriesController = require("./categoriesController")
 const { body, validationResult } = require("express-validator")
 
+const nameError = "Name can't be longer than 3 characters"
+const unitsError = "Max amount of units is 1 billion"
 
-const validateUser = [
+
+
+const validateItem = [
+    body("itemName").trim()
+        .isLength({ max: 3 }).withMessage(`${nameError}`),
+    body("units").trim()
+    .isInt({max: 1000000000}).withMessage(`${unitsError}`),
+    // checks if at least one category is selected:
+    body().custom(async (value, { req }) => {
+        console.log(req.body)
+        const hasSelectedCategory = Object.values(req.body).some(value => value === 'on')
+
+        if (!hasSelectedCategory) {
+            throw new Error('At least one category needs to be selected')
+        }
+
+
+    })
     
 ]
 
@@ -26,13 +45,37 @@ exports.newItemGet = async (req, res) => {
     })
 }
 
-exports.newItemPost = async (req, res) => {
-    const newItem = req.body;
-    console.log(newItem)
-    const selectedCategories = Object.entries(newItem)
-        .filter(([key,value]) => value === "on")
-        .map(([key]) => key);
-    await db.addNewItem(selectedCategories, newItem)
-    res.redirect("/")
-}
+// exports.newItemPost = async (req, res) => {
+//     const newItem = req.body;
+//     console.log(newItem)
+//     const selectedCategories = Object.entries(newItem)
+//         .filter(([key,value]) => value === "on")
+//         .map(([key]) => key);
+//     await db.addNewItem(selectedCategories, newItem)
+//     res.redirect("/")
+// }
+
+exports.newItemPost = [
+    validateItem,
+    async (req, res) => {
+        const errors = validationResult(req);
+        const categories = await categoriesController.allCategoriesGet();
+
+        if (!errors.isEmpty()) {
+            return res.status(400).render("newItem", {
+                title: "Add Item",
+                categories: categories,
+                errors: errors.array()
+            })
+        };
+
+        const newItem = req.body;
+        console.log(newItem)
+        const selectedCategories = Object.entries(newItem)
+            .filter(([key,value]) => value === "on")
+            .map(([key]) => key);
+        await db.addNewItem(selectedCategories, newItem)
+        res.redirect("/")
+    }
+]
 
